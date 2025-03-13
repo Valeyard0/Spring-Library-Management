c1500311805504cae6843b9419ac529b53cdc0d2
aliemrekaral <aliemrekaral@hotmail.com>
2025-03-13 21:54:06 +0300

    test

--- Mark/src/main/java/com/example/Mark/Config/Configs.java
+++ Mark/src/main/java/com/example/Mark/Config/Configs.java
@@ -1,4 +1,9 @@
 package com.example.Mark.Config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Configs {
 }
